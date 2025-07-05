e8c7fa119d59335f3ce2a815a12dbeaa02ef59c7
DenisPineda <denispinedal350@gmail.com>
2025-07-04 19:47:23 -0600

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const btnInfo = document.getElementById("btn-info");
-
-    btnInfo.addEventListener("click", function () {
-        alert("¡Gracias por tu interés en Dtania Coffee! Pronto tendremos más sorpresas para ti.");
-    });
-});
